66df50b37761fd5871bc7ce210039edef2155e65
Eremey Lazar <elazar1926@gmail.com>
2023-11-08 17:19:52 +0300

    connection is successful

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,29 +1,17 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-    public static Connection main(String[] args) {
+    public static void main (String[] args) {
         // реализуйте алгоритм здесь
+        Util util = new Util ();
+        util.getConnection();
 
-        final String DB_DRIVER = "org.h2.Driver";
-        final String DB_URL = "jdbc:h2:~/test";
-        final String DB_USERNAME = "ss";
-        final String DB_PASSWORD = "";
-
-        public Connection getConnection () {
-            Connection connection = null;
-            try {
-                connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-                System.out.println("Connection DONE");
-            } catch (SQLException e ) {
-                e.printStackTrace();
-                System.out.println("Connection Error");
-            }
-            return connection;
-            }
         }
     }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    final String DB_URL = "jdbc:mysql://localhost:3306/users";
+    final String DB_USERNAME = "root";
+    final String DB_PASSWORD = "Ьнакуувщь1001011";
+
+    public Connection getConnection () {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            System.out.println("Connection DONE");
+        } catch (SQLException e ) {
+            e.printStackTrace();
+            System.out.println("Connection Error");
+        }
+        return connection;
+    }
 }
