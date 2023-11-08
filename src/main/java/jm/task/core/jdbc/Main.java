9b99da7e6dde62c2532ada2df204c587ca80fe89
Eremey Lazar <elazar1926@gmail.com>
2023-11-08 16:39:46 +0300

    connection

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,29 @@
 package jm.task.core.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static Connection main(String[] args) {
         // реализуйте алгоритм здесь
+
+        final String DB_DRIVER = "org.h2.Driver";
+        final String DB_URL = "jdbc:h2:~/test";
+        final String DB_USERNAME = "ss";
+        final String DB_PASSWORD = "";
+
+        public Connection getConnection () {
+            Connection connection = null;
+            try {
+                connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+                System.out.println("Connection DONE");
+            } catch (SQLException e ) {
+                e.printStackTrace();
+                System.out.println("Connection Error");
+            }
+            return connection;
+            }
+        }
     }
-}
+
